32fc32a490107e5d7febb1c9df8b55c683935424
NileshThakur2003 <nileshthakurbgp@gmail.com>
2024-04-09 19:49:11 +0530

    commintin changes 9/04

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import {MEHeadingText } from './CustomComponents'
+import {ImageBox, MEHeadingText, MainCard, SearchBox } from './CustomComponents'
 import {MathComponent} from './MathJaxComponents';
 
 
@@ -9,7 +9,19 @@ function App() {
 
   return (
     <>
+    <div className='bg-gradient-to-bl  from-cyan-500 to-blue-500'>
     <MEHeadingText />
+
+    <SearchBox />
+    <div className='grid grid-col-* mx-5 mt-10'>
+      <MainCard content="Linear Algebra" url="https://images.pexels.com/photos/7794450/pexels-photo-7794450.jpeg?auto=compress&cs=tinysrgb&w=300" >
+        <ImageBox />
+      </MainCard>
+      <MainCard content="Multivariable Calculus"/>
+      <MainCard content="Sequences And Series"/>
+      <MainCard content="Statistics and Probablity"/>
+    </div>
+    </div>
     </>
   )
 }
